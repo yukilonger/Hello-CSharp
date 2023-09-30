9478acf936e35cfca92d5cb03fba2b0998539f33
児 <1047431389@qq.com>
2023-10-01 02:09:02 +0800

    supplements

---
+++ GRPC_Client/GrpcClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Utility
+{
+    public sealed class GrpcClient
+    {
+        private static readonly GrpcClient instance = new GrpcClient();
+        public Channel Channel { get; set; }
+        static GrpcClient() 
+        {
+            
+        }
+        private GrpcClient() 
+        {
+            Channel = new Channel("127.0.0.1", 10011, ChannelCredentials.Insecure);
+        }
+        public static GrpcClient Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
